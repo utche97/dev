fd8520862fb9c5eb5efc1bb19e629b043cc59b57
Anthony Taylor <utche97@gmail.com>
2020-05-04 14:00:35 -0500

    Update SetUserDisplayName.ps1

--- SetUserDisplayName.ps1
+++ SetUserDisplayName.ps1
@@ -24,6 +24,33 @@ Add-PSSnapin Microsoft.SharePoint.PowerShell -ErrorAction "SilentlyContinue" | O
     $site = Get-SPSite $path
     foreach($web in $site.AllWebs)
     {
-      
+      try{
+        $user = Get-SPUser -Identity "i:0#.w|Test-Domain|JohnDoe" -Web $web
+        #set the user display name
+        Set-SPUser -Identity "i:0#.w|Test-Domain|JohnDoe" -DisplayName "Doe, John H." -Web $web
+      }
+      catch
+      {
+        #move on to the next website
+      }
+      $site.Dispose()
     }
   }
+[xml]$s = Get-Content UpgradeSites.xml
+Write-Host " "
+Write-Host " "
+
+foreach ($SiteCollection in $s.Upgrade.SiteCollections.SiteCollection)
+{
+  $SCName = $SiteCollection.getAttribute("Name")
+  Echo $SCName
+  $WebApp = $SiteCollection.getAttribute("HostHeaderWebApplication")
+  Echo $webApp
+  $SiteURL = $SiteCollection.getAttribute("URL")
+  Echo $SiteURL
+  
+  $FullSCURLPath = $WebApp + $SiteURL
+  Echo "Name: $SCName -URL $FullSCURLPath"
+  
+  UpdateDisplayName($FullSCURLPath)
+}
